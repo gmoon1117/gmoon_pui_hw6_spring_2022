b4741e879439bc0f42d29ebbe46b6d3e7f8c5dfd
Gloria <hyojung.gm@gmail.com>
2022-03-21 11:00:12 -0400

    fixed js issues

--- main.js
+++ main.js
@@ -1,24 +1,50 @@
 //variables
 var a = ["Teatree Care Solution", "Soft Foot Peeling Socks", "Roselle Tea Eye Gel Patch"];
 var len = 3;
-//document.getElementById("shoppingcart").innerHTML = a;
-//len = a.length;
-//document.getElementById("shoppingcartlen").innerHTML = len;
 
 // add function when users press "add to cart"
-function add() {
-  a.push("Item");
+function add_to_cart() {
+  var name = document.getElementById("product_name").innerHTML
+  // var x = ????
+  // for (var i=0; i<x; i++) {
+  //   a.push(name);
+  // }
+
+  a.push(name);
   //document.getElementById("shoppingcart").innerHTML = a;
   len = a.length;
   document.getElementById("shoppingcartlen").innerHTML = len;
 }
 
-// function switch() {
-//  var Image_Id = document.getElementById('product3_image');
-//  if (Image_Id.src.match("images/pp1.png")) {
-//    Image_Id.src = "images/p2p1.png";
-//  }
-//  else {
-//    Image_Id.src = "images/pp1.png";
-//  }
-// }
+function switch_image(x) {
+  // current main product image
+ var Image_Id = document.getElementById('product3_image');
+ // switch to green tea
+ if (Image_Id.src.match("images/pp1.png") && x == 2) {
+   Image_Id.src = "images/p2p1.png";
+   document.getElementById('flavor').innerHTML="Green Tea";
+ }
+ // switch to roselle
+ else if (Image_Id.src.match("images/p2p1.png") && x == 1) {
+   Image_Id.src = "images/pp1.png";
+   document.getElementById('flavor').innerHTML="Roselle";
+ }
+}
+
+function increaseCount(a, b) {
+  var input = b.previousElementSibling;
+  var value = parseInt(input.value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  input.value = value;
+}
+
+function decreaseCount(a, b) {
+  var input = b.nextElementSibling;
+  var value = parseInt(input.value, 10);
+  if (value > 1) {
+    value = isNaN(value) ? 0 : value;
+    value--;
+    input.value = value;
+  }
+}
